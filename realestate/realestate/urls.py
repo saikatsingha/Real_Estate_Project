e07b233e08abed52b725015262b9bacd00370dd6
saikatsingha <saikatsingha@gmail.com>
2021-11-26 23:26:51 +0530

    rebuilding real_estate

--- realestate/realestate/urls.py
+++ realestate/realestate/urls.py
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
-# from django.urls import path, include, re_path
-# from django.views.generic import TemplateView
-# from django.conf import settings
-# from django.conf.urls.static import static
-# from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.contrib import admin
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
